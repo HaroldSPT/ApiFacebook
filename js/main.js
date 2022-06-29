7accfdd35c8fcdf00bad8b468eb26555b8c0809b
HaroldSPT <hspatino2@misena.edu.co>
2022-06-29 17:58:20 -0500

    Agregamos un sistema de favoritos al proyecto de la api de facebook

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
 const API = "https://graph.facebook.com/v14.0/";
-const AT = "EAAQUDXtOgSYBAKIOZC54BOEbCUMtUUnr9ee66HyZBf7qLHati0wFUaZAz3RSRZAUWop8xCWXm2pIjEhtyV6Risi5MShG5uZC5QbdHUUyLz80hrxnDo2PWXAwXlMhdBm4qFs5v64RTg6ftyLPKIm4In3slZABivoTfL19IrC4Q8ZCwAz3Iys2VuJ";
-const CS = "?fields=id,name,email,picture&access_token="
+const AT = "EAAQUDXtOgSYBAItn6w1RIuSVu8geOtwbQaMDbYsMZB2gwYc9cNUkEer4ZATBQVMBUstuMNx7Vo3gWDoO1jTRw0DzdkitxTyvIghxhzwrHk05ZA3xiRWZBQya062DHhidTwG6NNTFZCRpwpbl9ALtZAHirn8X1mkf1CmHCkSzQqtpWVGiMUiWJsG8yxpiGZAiaEZD";
+const CS = "?fields=id,name,picture&access_token="
 
 const app = Vue.createApp({
   data() {
     return {
       busqueda: null,
       error: null,
-      result: null
+      result: null,
+      favorite: new Map()
+    }
+  },
+
+  created() {
+    const SavedFavorites = JSON.parse(window.localStorage.getItem("myfav"))
+    if(SavedFavorites?.length){
+        const FavoriteRebuild = new Map(SavedFavorites.map(alias=>[alias.id,alias]))
+        this.favorite = FavoriteRebuild
+    }
+  },
+
+  computed: {
+    isFavorite(){
+      return this.favorite.has(this.result.id)
+    },
+
+    allFavorites(){
+      return Array.from(this.favorite.values())
     }
   },
 
@@ -16,15 +35,33 @@ const app = Vue.createApp({
       this.result = this.error = null
       try {
         const response = await fetch(API + this.busqueda + CS + AT)
-        //if(!response.ok) throw new Error("Usuario no econtrado")
+        if(!response.ok) throw new Error("Usuario no econtrado")
         const data = await response.json()
         console.log(data)
         this.result = data
       } catch (error) {
         this.error = error
-      /*} finally {
-        this.busqueda*/
+      } finally {
+        this.busqueda
       }
+    },
+
+    addFavorite(){
+      this.favorite.set(this.result.id, this.result)
+      this.updateStorage()
+    },
+
+    removeFavorite(){
+      this.favorite.delete(this.result.id)
+      this.updateStorage()
+    },
+
+    updateStorage(){
+      window.localStorage.setItem('myfav', JSON.stringify(this.allFavorites))
+    },
+
+    showFavorite(par){
+      this.result = par
     }
   }
 })
